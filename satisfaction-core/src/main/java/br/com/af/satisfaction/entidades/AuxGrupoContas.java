a622c95da66fbb8a3713b5c96109177c8f4d316c
filipe <filipe.muler@gmail.com>
2015-10-14 00:16:17 -0300

    mapeando tabelas

---
+++ satisfaction-core/src/main/java/br/com/af/satisfaction/entidades/AuxGrupoContas.java
@@ -0,0 +1,52 @@
+package br.com.af.satisfaction.entidades;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_AUX_GRUPOCONTAS")
+public class AuxGrupoContas {
+
+	private Long id;
+	private String nome;
+	private Date dataCadastro;
+	private Usuarios usuario;
+	private Date ultimaModificacao;
+
+	@Id
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Date getUltimaModificacao() {
+		return ultimaModificacao;
+	}
+
+	public void setUltimaModificacao(Date ultimaModificacao) {
+		this.ultimaModificacao = ultimaModificacao;
+	}
+
+}
